0cdce633fb084b108213cecff755cafa8d642626
Masayoshi <>
2023-07-10 00:33:26 +0900

    generateStaticParamsの記述を追加（SG用）

--- app/id/[id]/page.jsx
+++ app/id/[id]/page.jsx
@@ -1,3 +1,15 @@
+//Build時に実行される。SGで生成するときに必要（下記の場合、1のidページだけ作成される）
+export async function generateStaticParams() {
+  return [{ id: "1" }]; //この値がparams引数にいく
+
+  //普通は上記のように個別に指定するのではなく、下記のようにこの関数内ですべてのIDの配列を自動で作成して、SGで生成する。
+  // const posts = await fetch('https://.../posts').then((res) => res.json())
+
+  // return posts.map((post) => ({
+  //   slug: post.slug,
+  // }))
+}
+
 const Page = async ({ params }) => {
   // console.log(params); //paramsには[〇〇]に対応したものが入る。今回は[id]なのでid
   const id = params.id;
