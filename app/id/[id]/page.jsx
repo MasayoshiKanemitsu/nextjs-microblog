aeb7e2b612fcf938a4bd99ab3656be7ab308402f
Masayoshi <>
2023-07-09 17:51:19 +0900

    ダイナミックルーティング。jsonplaceholderに紐付けしてみた

--- app/id/[id]/page.jsx
+++ app/id/[id]/page.jsx
@@ -1,9 +1,13 @@
-const Page = ({ params }) => {
+const Page = async ({ params }) => {
   // console.log(params); //paramsには[〇〇]に対応したものが入る。今回は[id]なのでid
   const id = params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const user = await res.json();
+
   return (
     <>
       <p>ID is {id}.</p>
+      <p>User：{user.name}</p>
     </>
   );
 };
